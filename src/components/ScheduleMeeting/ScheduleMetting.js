739869c033815253843c16c8ed5a1453bce024e3
Muhammad Asad <108513936+asad78698@users.noreply.github.com>
2024-07-06 18:53:25 +0500

    Update ScheduleMetting.js

--- src/components/ScheduleMeeting/ScheduleMetting.js
+++ src/components/ScheduleMeeting/ScheduleMetting.js
@@ -11,7 +11,11 @@ export default function ScheduleMetting() {
     const handleContactForm = async (e) => {
         e.preventDefault()
 
+<<<<<<< Updated upstream
         const res = await fetch('https://cafevista-backend.vercel.app/', {
+=======
+        const res = await fetch('https://cafevista-frontend.vercel.app/home', {
+>>>>>>> Stashed changes
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
